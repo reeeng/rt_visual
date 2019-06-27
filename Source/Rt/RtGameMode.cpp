34748fd672d5b889fe95d066977a74ec715cb38f
Adipa Wijayathilaka <adipamihi@gmail.com>
2019-06-26 20:39:03 -0400

    Project init

---
+++ Source/Rt/RtGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "RtGameMode.h"
+#include "RtHUD.h"
+#include "RtCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ARtGameMode::ARtGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ARtHUD::StaticClass();
+}
