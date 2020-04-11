cbae718d4e420c4ae5943b91b930604b3298cb05
Adipa Wijayathilaka <adipamihi@gmail.com>
2020-04-11 17:00:11 -0400

    Added no ar scene + added delete and change obj type to loop.

--- unity/rt_unity/Assets/Scripts/ItemManager.cs
+++ unity/rt_unity/Assets/Scripts/ItemManager.cs
@@ -14,8 +14,7 @@ namespace UnityTemplateProjects
     public class ItemManager : MonoBehaviour, IItemManager
     {
         public ManagerState State { get; private set; }
-        [SerializeField] 
-        public List<GameObject> itemPrefabs;
+        [SerializeField] public List<GameObject> itemPrefabs;
 
         private Dictionary<int, GameObject> itemPrefabDict = new Dictionary<int, GameObject>();
 
@@ -24,8 +23,8 @@ namespace UnityTemplateProjects
         public void Startup()
         {
             State = ManagerState.Active;
-            
-            foreach(var item in itemPrefabs)
+
+            foreach (var item in itemPrefabs)
             {
                 var itemPrefabComp = item.GetComponent<ItemPrefab>();
                 itemPrefabDict.Add(itemPrefabComp.Id, item);
@@ -34,30 +33,58 @@ namespace UnityTemplateProjects
 
         public void OnFetchItems(Dictionary<int, Item> newItems)
         {
-            var itemsInResp = new int[]{};
+            var itemsInResp = new List<int>();
 
             foreach (var e in newItems)
             {
-                itemsInResp.Append(e.Key);
+                itemsInResp.Add(e.Key);
 
                 if (!_items.ContainsKey(e.Key))
                 {
                     var itemType = e.Value.type;
                     var newItemSpawned = Instantiate(itemPrefabDict[itemType]);
                     _items.Add(e.Key, newItemSpawned);
+
+                    Debug.Log(("Spawned item"));
+                }
+                else
+                {
+                    // Destroy and replace object with correct object of type
+                    if (e.Value.type != _items[e.Key].GetComponent<ItemPrefab>().Id)
+                    {
+                        ReplaceItem(e.Key, itemPrefabDict[e.Value.type]);
+                    }
                 }
 
                 _items[e.Key].GetComponent<Transform>().position = e.Value.position + AnchorPoint;
                 _items[e.Key].GetComponent<Transform>().eulerAngles = e.Value.rotation;
             }
 
-            var itemsToRemove = _items.Keys.Where(key => !itemsInResp.Contains(key));
+
+            // Remove removed items
+            var itemsToRemove = _items.Keys.Where(key => !itemsInResp.Contains(key)).ToList();
 
             foreach (var key in itemsToRemove)
             {
-                Destroy(_items[key]);
-                _items.Remove(key);
+                RemoveItem(key);
             }
         }
+
+        private void ReplaceItem(int key, GameObject prefab)
+        {
+            var position = _items[key].transform.position;
+            var rotation = _items[key].transform.rotation;
+
+            RemoveItem(key);
+            _items.Add(key, Instantiate(prefab, position, rotation));
+        }
+
+        private void RemoveItem(int key)
+        {
+            Destroy(_items[key]);
+            _items.Remove(key);
+        }
+
+        public Vector3 AnchorPoint { get; set; } = Vector3.zero;
     }
 }
